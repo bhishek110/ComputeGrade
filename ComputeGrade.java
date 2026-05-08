c3b72d41aa0428daeb58357195ff67117577f65a
██  A ＢＩＳ Ｈ Ξ Ｋ ██ <160328652+bhishek110@users.noreply.github.com>
2026-05-08 10:43:28 +0530

    Update ComputeGrade.java

--- ComputeGrade.java
+++ ComputeGrade.java
@@ -1,14 +1,18 @@
 public class ComputeGrade {
 
-    public int marks(int mark){
-        if(mark >= 90){
-            System.out.println("A");
-        }elseif(mark>=75 && mark<=89){
-            System.out.println("B");
-        }elseif(mark >=50 && mark <=74){
-            System.err.println("C");
-        }else{
-            System.err.println("Fail");
+    public String marks(int mark) {
+
+        if (mark >= 90) {
+            return "A";
+        } 
+        else if (mark >= 75 && mark <= 89) {
+            return "B";
+        } 
+        else if (mark >= 50 && mark <= 74) {
+            return "C";
+        } 
+        else {
+            return "Fail";
         }
     }
 }
